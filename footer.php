76a7343c402985138e3e49d170cab2eadb9dcab9
Saad <info.cryptocurious@gmail.com>
2024-05-11 17:29:20 +0500

    Footer completed

--- footer.php
+++ footer.php
@@ -1,6 +1,37 @@
+<!-- ====== Footer Page ====== -->
 <footer class="footer">
- </footer> <!-- =============== Icons Script ============== --> <script src="https://cdn.lordicon.com/lordicon.js"></script>
-
+    <div class="footer-container">
+        <div class="parent-element">
+        <ul>
+            <li><h3>Rawayah</h3></li>
+            <li class="description"><p>Rawayah is Premimum Footwear brand offering high quality
+                and stylish shoes for women and empowering 
+                the confidence.
+            </p></li>
+                <ul class="contact-items">
+                    <li><i class="fa-regular fa-envelope"></i> <span>support.info@rawayah.com</span></li>
+                    <li><i class="fa-solid fa-phone"></i> <span>+92-03147654321</span></li>
+                    <li><i class="fa-regular fa-clock"></i> <span>8:00 AM - 12:00 PM</span></li>
+                </ul>
+        </ul>
+        <ul class="customer-service">
+            <li><h3>Customer Service</h3></li>
+            <li><a href="#">Exchange and Refund Policy</a></li>
+            <li><a href="#">Shipping Policy</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Term of Services</a></li>
+            <li><a href="#">FAQs</a></li>
+        </ul>
+        <ul class="newsletter">
+            <li><h3>Newsletter</h3></li>
+            <li><p>Subscribe to receive Exclusive deals, and more</p></li>
+            <li><input type="email" required></li>
+            <li><button type="submit">Subscribe</button></li>
+        </ul>
+        </div>
+    </div>
+</footer> 
+<!-- =============== Icons Script ============== --> <script src="https://cdn.lordicon.com/lordicon.js"></script>
 <!-- ============= Card Slider JS Script ================ -->
 <script src="./js/Card-slider.js"></script>
 <?php wp_footer(); ?>
